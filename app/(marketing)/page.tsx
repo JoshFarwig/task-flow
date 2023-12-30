58adeff4917649fa241edf431a016eebe5df9829
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2023-12-30 11:02:39 -1000

    completed logo

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,38 @@
-import Link from "next/link";
-import { Medal } from "lucide-react"; 
+import Link from "next/link"; 
+import localFont from "next/font/local";  
+import { Poppins } from "next/font/google";
+import { Medal, Weight } from "lucide-react"; 
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";  
+import { text } from "stream/consumers";
+
+const headingFont = localFont({ 
+    src: "../../public/fonts/postcss.config.woff2"
+})
+
+const textFont = Poppins({
+    subsets: ["latin"], 
+    weight: [ 
+        "100", 
+        "200", 
+        "300", 
+        "400",
+        "500",
+        "600", 
+        "700", 
+        "800", 
+        "900"
+    ]
+})
 
 const MarketingPage = () => { 
     return ( 
         <div className="flex items-center justify-center flex-col"> 
-            <div className="flex items-center justify-cetner flex-col">
+            <div className={cn(
+                    "flex items-center justify-cetner flex-col", 
+                    headingFont.className,
+                )}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100
                 text-amber-700 uppercase rounded-full">
                     <Medal className="h-6 w-6 mr-2" />
@@ -19,10 +45,13 @@ const MarketingPage = () => {
                 organize and execute. 
                 </div>  
             </div> 
-            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto"> 
-                Collaborate, manage Projects, and reach new productivity peaks. 
-                From high mountains to the home office the way your team works is unique - accomplish 
-                it all with Task Flow 
+            <div className={cn(
+                    "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto", 
+                    textFont.className, 
+                )}> 
+                    Collaborate, manage Projects, and reach new productivity peaks. 
+                    From high mountains to the home office the way your team works is unique - accomplish 
+                    it all with Task Flow 
             </div> 
             <Button className="mt-6" size="lg" asChild>
                 <Link href="/signup"> 
