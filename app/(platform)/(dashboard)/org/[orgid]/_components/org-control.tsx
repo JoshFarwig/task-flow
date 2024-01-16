c2b5b62253ba11b9ab4213b8ee4a28cf1fbd49f6
Joshua Farwig <joshfarwig@gmail.com>
2024-01-15 21:15:13 -0800

    org control through url, adding packages

--- app/(platform)/(dashboard)/org/[orgid]/_components/org-control.tsx
+++ app/(platform)/(dashboard)/org/[orgid]/_components/org-control.tsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { useParams } from "next/navigation";
 import { useOrganizationList } from "@clerk/nextjs"; 
 
+// Component to control client side URL changes when switching organizations 
+// uses clerk's setActive Function. 
 export const OrgControl = () => { 
 
     const params = useParams(); 
@@ -11,7 +13,6 @@ export const OrgControl = () => {
      
     useEffect(() => {  
         if (!setActive) return;
-
         setActive({ 
             organization: params.orgid as string,
         })
