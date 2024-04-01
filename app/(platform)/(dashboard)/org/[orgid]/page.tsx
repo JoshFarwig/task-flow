1c4315c0a7f3f8afed0c35c0ed5201ed509aded6
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2024-03-31 17:37:13 -0700

    added a pop-over / custom button to create a board w/ toasts

--- app/(platform)/(dashboard)/org/[orgid]/page.tsx
+++ app/(platform)/(dashboard)/org/[orgid]/page.tsx
@@ -1,4 +1,6 @@
+import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 
 
 const OrgIdPage = async () => {  
@@ -6,6 +8,10 @@ const OrgIdPage = async () => {
     return ( 
         <div className="w-full mb-20">
             <Info />
+            <Separator className="my-4"/> 
+            <div className="px-2 md:px-4"> 
+                <BoardList />
+            </div>
         </div>
     );
 }; 
