2808c0fa7280b10635ac81faf363f53e8f8cd23e
Joshua Farwig <joshfarwig@gmail.com>
2024-04-15 15:52:59 -0700

    started board page, creating update server action

--- app/(platform)/(dashboard)/org/[orgid]/layout.tsx
+++ app/(platform)/(dashboard)/org/[orgid]/layout.tsx
@@ -1,5 +1,16 @@
-import { OrgControl } from "./_components/org-control";
 
+import { startCase } from "lodash";
+
+import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs";
+
+export async function generateMetadata() { 
+    const { orgSlug } = auth(); 
+    
+    return { 
+        title: startCase(orgSlug || "organization"),
+    }
+}
 const OrgIdLayout = ({ 
     children
 } : { 
